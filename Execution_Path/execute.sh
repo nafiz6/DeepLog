12870e8ab4d8383b671a69d5fccb19d5f3ff2a10
Nafiz Imtiaz Chowdhury <nafiz6@yahoo.com>
2021-11-29 23:59:10 +0600

    Update execute.sh

--- Execution_Path/execute.sh
+++ Execution_Path/execute.sh
@@ -5,7 +5,7 @@ path2="../Dataset/Linux/Client/Client_structured/log_value_vector.csv"
 path3="../Dataset/Linux/Client/Client_structured_com/Integrated_structured_log.csv"
 path4="../Dataset/Linux/Client/Client_structured_com/log_value_vector.csv"
 
-python3 Execution_Path_Anomaly_para.py --p1 $path1 --p2 $path2 --p3 $path3 --p4 $path4
+python3 Execution_Path_Anomaly.py --p1 $path1 --p2 $path2 --p3 $path3 --p4 $path4
 
 
 
